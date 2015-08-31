90b7466a5120efa3da080f9030f0f96bcb8bd44b
Jayden Seric <Jayden.Seric@cheproximity.com.au>
2015-08-31 22:52:54 +1000

    Big revision.
    - Removed the demo until it can be re-written to reflect recent changes.
    - Revised the way callbacks intended to only fire once per run can be setup, removing the `addUniqueAction` method. A per-run session object can be accessed as a callback argument, handy for creating custom flags to ensure certain things only happen once per run.
    - Fixed an issue where typing a valid hash directly into the URL bar would not trigger a Hurdler run.
    - Updated documentation.

--- README.md
+++ README.md
@@ -1,67 +1,53 @@
-# ![Hurdler logo](http://jaydenseric.com/shared/hurdler-logo.svg)
+# ![Hurdler](http://jaydenseric.com/shared/hurdler-logo.svg)
 
-Hurdler enables hash links to web page content that is hidden beneath layers of interaction. Ultra-lightweight with zero dependancies.
+Hurdler enables hash links to web page content that is hidden beneath layers of interaction. Ultra-lightweight plain JS.
 
-On document ready or whenever the URL fragment identifier changes, Hurdler finds the hash target element and runs the configured tests. Each passing test triggers an action. Tests are repeated for each ansestor up the DOM.
+Setup simple tests identifying components along with callbacks for what you want to happen when they are encountered. When a URL hash is set all tests are run against the target element and ancestors.
 
-Native scroll-jump behavior when a same-page hash link is clicked and a test passes is prevented with `event.preventDefault()`.
-
-## Demo
-
-Try out *demo.html* [via RawGit](http://rawgit.com/jaydenseric/Hurdler/master/demo.html).
+Native scroll-jump behaviour is prevented for same-page hash links that pass a test.
 
 ## Browser support
 
 Evergreen browsers.
 
-## API
+Be sure to include [a polyfill](https://plainjs.com/javascript/traversing/get-closest-element-by-selector-39) for [`Element.closest()`](https://developer.mozilla.org/docs/Web/API/Element/closest).
+
+## Usage
 
 ### `new Hurdler()`
 
 Constructs a new Hurdler instance.
 
-### `hurdler.addUniqueAction(id, action)`
-
-Adds a new unique action that can be triggered once per hash change by multiple tests.
-
-Param  | Type       | Description
------- | ---------- | -------------------------------------------------
-id     | `string`   | Unique action ID.
-action | `function` | Action intended to run only once per hash change.
-
-### `hurdler.addTest(options)`
+### `hurdler.addTest(test, callback)`
 
 Adds a new test.
 
-Param                   | Type       | Description
------------------------ | ---------- | -----------------------------------------------------------
-options                 | `object`   | Options for the test.
-options.id              | `string`   | Unique test ID.
-options.test            | `function` | Must return a boolean determining if the action should run.
-options.action          | `function` | Action to run if the test passes.
-[options.uniqueActions] | `array`    | Optional: Unique actions to run if the test passes.
+Parameter | Type | Description
+--- | --- | ---
+test | function | Test returning a Boolean if the callback should fire.
+callback | function | Callback to fire if the test passes. Passed a per-run session object argument, handy for creating custom flags to ensure certain things only happen once per run.
 
 ### `hurdler.run(event)`
 
-Runs tests and actions for the current URL hash.
+Runs tests and callbacks for the current URL hash. Use this after all your tests have been added and the document is ready.
 
-Param   | Type     | Description
-------- | -------- | ----------------------------------------------------
-[event] | `object` | Optional: Event to prevent default if a test passes.
+Parameter | Type | Description
+---| --- | ---
+[event] | object | Optional: Event to prevent default if a test passes.
 
 ### `hurdler.setHash(id, event)`
 
 Sets the URL hash and runs the tests.
 
-Param   | Type     | Description
-------- | -------- | ----------------------------------------------------
-id      | `string` | A DOM element ID.
-[event] | `object` | Optional: Event to prevent default if a test passes.
+Parameter | Type | Description
+--- | --- | ---
+id | string | A DOM element ID.
+[event] | object | Optional: Event to prevent default if a test passes.
 
 ### `hurdler.clearHash(id)`
 
 Clears the URL hash if a particular hash is active.
 
-Param   | Type     | Description
-------- | -------- | -----------------
-id      | `string` | A DOM element ID.
+Parameter | Type | Description
+--- | --- | ---
+id | string | A DOM element ID.
--- hurdler.js
+++ hurdler.js
@@ -9,79 +9,56 @@
  */
 function Hurdler() {
   var self = this;
-  self.uniqueActions = [];
   self.tests = [];
   // Ensure URL hash change triggers tests
-  window.addEventListener('hashchange', self.run);
+  window.addEventListener('hashchange', function() { self.run() });
   // Prevent native scroll jump for same-page hash links that match a test
   document.addEventListener('click', function(event) {
+    var link = event.target.closest('a');
     if (
-      event.target.hash
-      && event.target.search == location.search
-      && (event.target.pathname == location.pathname || '/' + event.target.pathname == location.pathname)
-      && event.target.host == location.host
-    ) self.setHash(event.target.hash.substr(1), event);
+      link
+      && link.hash
+      && link.search == location.search
+      && (link.pathname == location.pathname || '/' + link.pathname == location.pathname)
+      && link.host == location.host
+    ) self.setHash(link.hash.substr(1), event);
   });
 };
 
 /**
- * Adds a new unique action that can be triggered once per hash change by multiple tests.
+ * Adds a new test. A per-run session object can be accessed as a callback argument, handy for creating custom flags to ensure certain things only happen once per run.
  * @method
- * @param {string} id - Unique action ID.
- * @param {function} action - Action intended to run only once per hash change.
+ * @param {function} test - Test returning a boolean if the callback should fire.
+ * @param {function} callback - Callback to fire if the test passes. Passed a per-run session object.
  */
-Hurdler.prototype.addUniqueAction = function(id, action) {
-  this.uniqueActions[id] = action;
+Hurdler.prototype.addTest = function(test, callback) {
+  this.tests.push({
+    test     : test,
+    callback : callback
+  });
 };
 
 /**
- * Adds a new test.
- * @method
- * @param {object} options - Options for the test.
- * @param {string} options.id - Unique test ID.
- * @param {function} options.test - Must return a boolean determining if the action should run.
- * @param {function} options.action - Action to run if the test passes.
- * @param {array} [options.uniqueActions] - Optional: Unique actions to run if the test passes.
- */
-Hurdler.prototype.addTest = function(options) {
-  this.tests.push(options);
-};
-
-/**
- * Runs tests and actions for the current URL hash.
+ * Runs tests and callbacks for the current URL hash.
  * @method
  * @param {object} [event] - Optional: Event to prevent default if a test passes.
  */
 Hurdler.prototype.run = function(event) {
-  var self = this;
   // Abandon if no URL hash
   if (!location.hash) return;
-  // Record used unique actions
-  var usedUniqueActions = [];
+  var self    = this,
+      element = document.querySelector(location.hash);
+      session = {};
   // Start at the hash target and loop up the DOM
-  var node = document.querySelector(location.hash);
-  for (; node && node !== document; node = node.parentNode) {
-    // Run the tests on this DOM node
-    for (var id in self.tests) {
-      if (self.tests[id].test.call(node)) {
-        // Test passed, prevent default event action (if supplied)
+  for (; element && element !== document; element = element.parentNode) {
+    // Run all tests on this DOM element
+    for (var i in self.tests) {
+      // Check test passes
+      if (self.tests[i].test.call(element, session)) {
+        // If event supplied, prevent the default action
         if (event !== undefined) event.preventDefault();
-        // Run the test passed action
-        self.tests[id].action.call(node);
-        // Check for unique actions
-        if (self.tests[id].uniqueActions !== undefined) {
-          for (var index in self.tests[id].uniqueActions) {
-            // Get the unique action ID
-            var uniqueActionID = self.tests[id].uniqueActions[index];
-            // Check unique action unused
-            if (usedUniqueActions.indexOf(uniqueActionID) == -1) {
-              // Record unique action as used
-              usedUniqueActions.push(uniqueActionID);
-              // Run unique action
-              self.uniqueActions[uniqueActionID].call(node);
-            }
-          }
-        }
+        // Run the callback
+        self.tests[i].callback.call(element, session);
       }
     }
   }
